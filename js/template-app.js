a982cb47e8900f9a77b1e191e72558536bff8a84
unknown <Mikeslim@Mikeslim-Tablet.(none)>
2013-03-28 02:35:01 +0300

    scroller

--- js/template-app.js
+++ js/template-app.js
@@ -1,36 +1,31 @@
-
-function scroll(){
-	var theScroll;
-    theScroll = new iScroll('wrapper');
-    $('#tab-bar a').on('click', function(e){
-        e.preventDefault();
-        var nextPage = $(e.target.hash);
-        $("#pages .current").removeClass("current");
-        nextPage.addClass("current");
-    });
-}
-//document.addEventListener('DOMContentLoaded', scroll, false);
 document.addEventListener('deviceready', scroll, false);
 
+function scroll() {
+	var theScroll;
+	theScroll = new iScroll('wrapper');
+	$('#tab-bar li a').click(function(e) {
+		e.preventDefault();
+		var nextPage = $(e.target.hash);
+		$("#pages .current").removeClass("current");
+		nextPage.addClass("current");
+	});
+}
+// document.addEventListener('DOMContentLoaded', scroll, false);
 
-//function onBodyLoad()
-//{
-//document.addEventListener("deviceready", scroll, false);
-//}
-
-
-
-
+// function onBodyLoad()
+// {
+// document.addEventListener("deviceready", scroll, false);
+// }
 
 function page(toPage) {
-    var toPage = $(toPage),
-    fromPage = $("#pages .current");
-    if(toPage.hasClass("current") || toPage === fromPage) {
-        return;
-    };
-    toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
-        fromPage.removeClass("current fade out");
-        toPage.removeClass("fade in")
-    });
-    fromPage.addClass("fade out");
+	var toPage = $(toPage), fromPage = $("#pages .current");
+	if (toPage.hasClass("current") || toPage === fromPage) {
+		return;
+	}
+	;
+	toPage.addClass("current fade in").one("webkitAnimationEnd", function() {
+		fromPage.removeClass("current fade out");
+		toPage.removeClass("fade in")
+	});
+	fromPage.addClass("fade out");
 }
