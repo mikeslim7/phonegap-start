d065dffbbc1cb02abf1eeffa8deea0c0ff50e16d
unknown <Mikeslim@Mikeslim-Tablet.(none)>
2013-03-28 03:50:09 +0300

    different listener

--- js/template-app.js
+++ js/template-app.js
@@ -5,43 +5,21 @@
  * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-var app = {
-	initialize : function() {
-		this.bind();
-	},
-	bind : function() {
-		document.addEventListener('deviceready', this.deviceready, false);
-	},
-	deviceready : function() {
-		alert('device is ready');
-		$('#tab-bar li a').click(function() {
 
-			alert($(this).attr('href'));
-			$("#pages div.current").removeClass("current")
-			$($(this).attr('href')).addClass("current");
+window.addEventListener('load', function() {
+	document.addEventListener('deviceready', function() {
+		alert("PhoneGap is now loaded!");
+		alert($("#pages div.current").attr('id'));
 
-			// e.preventDefault();
-			// alert(e.target.hash);
-			// app.ViewPage(e.target.hash);
-			/*
-			 * var nextPage = $(e.target.hash); $("#pages .current").removeClass("current"); nextPage.addClass("current");
-			 */
-		});
-	},
-	ViewPage : function(toPage) {
-		var toPage = $(toPage), fromPage = $("#pages .current");
+	}, false);
+}, false);
 
-		if (toPage.hasClass("current") || toPage === fromPage) {
-			alert('returning');
-			return;
-		}
-
-		alert('returning not');
-		toPage.addClass("current fade in").one("webkitAnimationEnd", function() {
-			fromPage.removeClass("current fade out");
-			toPage.removeClass("fade in");
-		});
-		fromPage.addClass("fade out");
-	}
-
-};
+/*
+ * var app = { initialize : function() { this.bind(); }, bind : function() { document.addEventListener('deviceready', this.deviceready, false); }, deviceready : function() { alert('device is ready'); $('#tab-bar li a').click(function() {
+ * 
+ * alert($(this).attr('href')); $("#pages div.current").removeClass("current") $($(this).attr('href')).addClass("current"); // e.preventDefault(); // alert(e.target.hash); // app.ViewPage(e.target.hash); // // var nextPage = $(e.target.hash); $("#pages .current").removeClass("current"); nextPage.addClass("current"); // }); }, ViewPage : function(toPage) { var toPage = $(toPage), fromPage = $("#pages .current");
+ * 
+ * if (toPage.hasClass("current") || toPage === fromPage) { alert('returning'); return; }
+ * 
+ * alert('returning not'); toPage.addClass("current fade in").one("webkitAnimationEnd", function() { fromPage.removeClass("current fade out"); toPage.removeClass("fade in"); }); fromPage.addClass("fade out"); } };
+ */
