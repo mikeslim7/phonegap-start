271ec8f26409cd9957ccd23c89bb82a1eff4640f
Michael Kabugi <michael.kabugi@gmail.com>
2013-03-28 13:25:33 +0300

    Update template-app.js

--- js/template-app.js
+++ js/template-app.js
@@ -1,25 +1,42 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
 
-window.addEventListener('load', function() {
-	document.addEventListener('deviceready', function() {
-		alert("PhoneGap is now loaded!");
-		alert($("#pages div.current").attr('id'));
+    // Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-	}, false);
-}, false);
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+        //var myContact = navigator.contacts.create({"displayName": "Test User Cordova"});
+       // myContact.note = "This contact has a note.";
+        //console.log("The contact, " + myContact.displayName + ", note: " + myContact.note);
+        
+       // elem = document.getElementById('contact_create');
+       // elem.innerHTML = "The contact, " + myContact.displayName + ", note: " + myContact.note;
+        
+        // find all contacts with 'Test' in any name field
+       // var options = new ContactFindOptions();
+      //  options.filter="Chep";
+      //  var fields = ["displayName", "name"];
+      //  navigator.contacts.find(fields, onSuccess, onError, options);
+    }
 
-/*
- * var app = { initialize : function() { this.bind(); }, bind : function() { document.addEventListener('deviceready', this.deviceready, false); }, deviceready : function() { alert('device is ready'); $('#tab-bar li a').click(function() {
- * 
- * alert($(this).attr('href')); $("#pages div.current").removeClass("current") $($(this).attr('href')).addClass("current"); // e.preventDefault(); // alert(e.target.hash); // app.ViewPage(e.target.hash); // // var nextPage = $(e.target.hash); $("#pages .current").removeClass("current"); nextPage.addClass("current"); // }); }, ViewPage : function(toPage) { var toPage = $(toPage), fromPage = $("#pages .current");
- * 
- * if (toPage.hasClass("current") || toPage === fromPage) { alert('returning'); return; }
- * 
- * alert('returning not'); toPage.addClass("current fade in").one("webkitAnimationEnd", function() { fromPage.removeClass("current fade out"); toPage.removeClass("fade in"); }); fromPage.addClass("fade out"); } };
- */
+    // onSuccess: Get a snapshot of the current contacts
+    //
+    function onSuccess(contacts) {
+        for (var i=0; i<contacts.length; i++) {
+            //console.log("Display Name = " + contacts[i].displayName);
+            elem1 = document.getElementById('contact_find');
+            elem1.innerHTML = "Display Name = " + contacts[i].displayName;
+        }
+    }
+
+    // onError: Failed to get the contacts
+    //
+    function onError(contactError) {
+        elem1 = document.getElementById('contact_find');
+        elem1.innerHTML = "Error!";
+        //alert('onError!');
+    }
+
+ 
+
